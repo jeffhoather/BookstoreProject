2d2526d70169e58a8946d4fdbd4372d46a037068
Jeff Hoather <jhoather@byu.edu>
2022-02-27 19:42:43 -0700

    Add project files.

---
+++ BookstoreProject/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using BookstoreProject.Models;
+using BookstoreProject.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookstoreProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository repo;
+
+        public HomeController (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IActionResult Index(string bookType, int pageNum)
+        {
+            int pageSize = 10;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                .Where(b => b.Category == bookType || bookType == null)
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = (bookType == null 
+                        ? repo.Books.Count() 
+                        : repo.Books.Where(x => x.Category == bookType).Count()),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+        
+                return View(x);
+         }
+     }
+ }
+
---
+++ BookstoreProject/Models/ViewModels/BooksViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookstoreProject.Models.ViewModels
+{
+    public class BooksViewModel
+    {
+        public IQueryable<Book> Books { get; set; }
+
+        public PageInfo PageInfo { get; set; }
+
+
+    }
+}
